cbf0c767f7eed9f5e8dc7a7c9e5e4d7558502cb9
nibtr <nhthieu16@proton.me>
2026-01-11 20:22:22 +0700

    lazy string allocate in hashmap key

---
+++ src/bin/v3.rs
@@ -0,0 +1,45 @@
+use std::{
+    collections::{BTreeMap, HashMap},
+    f64,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() {
+    let f = File::open("data/measurements.txt").expect("file should exist");
+    let f = BufReader::new(f);
+
+    // (min, max, sum, count)
+    let mut stats: HashMap<String, (f64, f64, f64, usize)> = HashMap::with_capacity(10_000);
+
+    for line in f.lines().map_while(Result::ok) {
+        let (station, temperature) = line.split_once(";").expect("delimiter should be ;");
+        let temperature = temperature
+            .parse::<f64>()
+            .expect("should be a valid floating point");
+
+        let stats = match stats.get_mut(station) {
+            Some(stats) => stats,
+            None => stats
+                .entry(station.to_string())
+                .or_insert((f64::MAX, f64::MIN, 0.0, 0)),
+        };
+
+        stats.0 = stats.0.min(temperature);
+        stats.1 = stats.1.max(temperature);
+        stats.2 += temperature;
+        stats.3 += 1;
+    }
+
+    let stats = BTreeMap::from_iter(stats);
+    let mut stats = stats.into_iter().peekable();
+
+    print!("{{");
+    while let Some((station, (min, max, sum, count))) = stats.next() {
+        print!("{station}={min}/{:.1}/{max}", sum / (count as f64));
+        if stats.peek().is_some() {
+            print!(", ");
+        }
+    }
+    print!("}}")
+}
